0216b50bd400d4e55f4848c50e9cac6e11760f89
DucKien <91297650+DuckienDev@users.noreply.github.com>
2025-05-07 10:27:44 +0700

    create and get item

--- main.go
+++ main.go
@@ -1,50 +1,113 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 type TodoItem struct {
-	ID          int        `json:"id"`
-	Title       string     `json:"title"`
-	Description string     `json:"description"`
-	Stattus     string     `json:"status"`
-	CreatedAt   *time.Time `json:"created_at"`
-	UpdatedAt   *time.Time `json:"updated_at"`
+	Id          int        `json:"id" gorm:"column:id"`
+	Title       string     `json:"title" gorm:"column:title"`
+	Description string     `json:"description" gorm:"column:description"`
+	Status      string     `json:"status" gorm:"column:status"`
+	CreatedAt   *time.Time `json:"created_at" gorm:"column:created_at"`
+	UpdatedAt   *time.Time `json:"updated_at" gorm:"column:updated_at"`
 }
 
+func (TodoItem) TableName() string { return "todo_items" }
+
+type TodoItemCreation struct {
+	Id          int    `json:"-" gorm:"column:id"`
+	Title       string `json:"title" gorm:"column:title"`
+	Description string `json:"description" gorm:"column:description"`
+	Status      string `json:"status" gorm:"column:status"`
+}
+
+func (TodoItemCreation) TableName() string { return TodoItem{}.TableName() }
+
 func main() {
-	fmt.Println("Hello, World!")
 
-	now := time.Now().UTC()
+	godotenv.Load()
 
-	item := TodoItem{
-		ID:          1,
-		Title:       "This is item 1",
-		Description: "Decription 1",
-		Stattus:     "Doing",
-		CreatedAt:   &now,
-		UpdatedAt:   nil,
-	}
+	db, err := gorm.Open(mysql.Open(os.Getenv("DB_URL")), &gorm.Config{})
 
-	jsonData, err := json.Marshal(item)
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatalln(err)
 	}
-
-	fmt.Println(string(jsonData))
 
 	r := gin.Default()
+	// CRUD : Create , Read , Update, Delete\
+	// POST /v1/items/  (create new item)
+	// GET /v1/items/   (list items)  /v1/items?page=1
+	// GET /v1/items/:id (get item detail by id)
+	// (PUT  || PATCH) /v1/items/:id  (update an item by id)
+	// DELETE /v1/items/:id  (delete item by id)
+
+	v1 := r.Group("/v1")
+	{
+		items := v1.Group("/items")
+		{
+			items.POST("", CreateItems(db))
+			items.GET("")
+			items.GET("/:id", GetItem(db))
+			items.PATCH("/:id")
+			items.DELETE("/:id")
+		}
+	}
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": item,
+			"message": "Connected success",
 		})
 	})
 	r.Run(":3000")
+}
+
+func CreateItems(db *gorm.DB) func(*gin.Context) {
+	return func(ctx *gin.Context) {
+		var data TodoItemCreation
+		if err := ctx.ShouldBind(&data); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		if data.Status == "" {
+			data.Status = "Doing"
+		}
+
+		if err := db.Create(&data).Error; err != nil {
+
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"data": data.Id,
+		})
+	}
+}
+
+func GetItem(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
+		var data TodoItem
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		// data.Id = id
+		db.Where("id = ?", id).First(&data)
+	}
 }
